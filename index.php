355196714dda66a8ea83a4a822ab53d436c56e07
Kaung Htun Thant <3199busi@gmail.com>
2022-12-10 23:07:16 +0630

    bootstrap added

--- index.php
+++ index.php
@@ -3,43 +3,36 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link
+         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+         rel="stylesheet"
+         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+         crossorigin="anonymous"
+      />
 	<title>Login</title>
 </head>
 <body>
-	<div class="container">
+	<div class="container mt-5">
 		<div class="row">
-			<div class="col-md-3"></div>
-			<div class="col-md-6">
-				<table>
-					<form action="backend.php" method="post">
-						<tr>
-							<th>Login Form</th>
-						</tr>
-						<tr>
-							<td>
-								<label>Enter username or email</label>
-							</td>
-							<td>
-								<input type="text" name="username">
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<label>Enter password</label>
-							</td>
-							<td>
-								<input type="text" name="password">
-							</td>
-						</tr>
-						<tr>
-							<th>
-								<input type="submit" name="login">
-							</th>
-						</tr>
-					</form>
-				</table>
+			<div class="col-md-6 col-sm-9 col-xs-12 m-auto mt-5 card shadow px-3 py-5">
+				<form class="form" action="backend.php" method="post">
+						<h3 class="text-center"><u>Login Form</u></h3>
+						<div class="mt-5">
+							<input class="form-control text-center" type="text" name="username" placeholder="Enter username">
+						</div>
+						<br>
+						<div class="mt-2">
+							<input class="form-control text-center" type="password" name="password" placeholder="Enter password">
+						</div>
+						<br>
+						<hr>
+						<br>
+						<div class="text-center">
+							<input type="submit" name="login" class="form-control btn btn-primary">
+						</th>
+					</tr>
+				</form>
 			</div>
-			<div class="col-md-3"></div>
 		</div>
 	</div>
 </body>
